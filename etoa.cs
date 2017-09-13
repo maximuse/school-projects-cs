d9882dbe71c1598255bc5ab4374bd2601488b389
László Kővári <maximuse@windowslive.com>
2017-09-13 13:08:36 +0200

    ko

---
+++ etoa.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace etoa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Szöveg: ");
+            int c = 0;
+            char[] a = {
+                'a',
+                'á',
+                'e',
+                'é',
+                'i',
+                'í',
+                'o',
+                'ó',
+                'ö',
+                'ő',
+                'u',
+                'ú',
+                'ü',
+                'ű'
+            };
+            string s = Console.ReadLine();
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (a.Contains(s[i]))
+                {
+                    c++;
+                }
+            }
+            s = s.Replace("e", "a");
+            Console.WriteLine("A szövegben " + c + "db magánhangzó betű van.");
+            Console.WriteLine(s);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ko.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ko
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Föld / Hold (1 / 2): ");
+            int p = Convert.ToInt32(Console.ReadLine());
+            double t = 0;
+            Console.Write("Magasság: ");
+            int s = Convert.ToInt32(Console.ReadLine());
+
+            if (p == 1)
+            {
+                t = Math.Round(Math.Sqrt((2 * s) / 9.81), 3);
+            }
+            else
+            {
+                t = Math.Round(Math.Sqrt((2 * s) / (9.81 / 6)), 3);
+            }
+
+            Console.WriteLine("Idő: " + t);
+            Console.ReadKey();
+        }
+    }
+}
