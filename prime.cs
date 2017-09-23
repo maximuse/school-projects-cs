73c3e41f662dd497f78e8c46aecc07d449b89c8d
László Kővári <maximuse@windowslive.com>
2017-09-23 11:24:09 +0200

    prime

---
+++ prime.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.Title = "Is this a prime number?";
+
+            Console.Write("Number: ");
+            string r = Console.ReadLine();
+            uint n,
+                 x = 0;
+
+            if (!uint.TryParse(r, out n))
+            {
+                Console.WriteLine("Wrong value entered!");
+            }
+            else
+            {
+                n = Convert.ToUInt32(r);
+
+                for (uint i = 1; i <= n; i++)
+                {
+                    if (n % i == 0)
+                    {
+                        x++;
+                    }
+
+                    if (x > 2)
+                    {
+                        break;
+                    }
+                }
+
+                if (x > 2)
+                {
+                    Console.WriteLine("The " + n + " is not a prime number!");
+                }
+                else
+                {
+                    Console.WriteLine("The " + n + " is a prime number!");
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
